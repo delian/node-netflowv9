47e399b3448f205744b001a49b22505bedb070f1
Delian Delchev <delian.delchev@gmail.com>
2014-11-13 22:23:59 +0000

    Scope fix is introduced
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- netflowv9-new.js
+++ netflowv9-new.js
@@ -37,101 +37,101 @@ var decStringRule = {
 };
 
 var nfTypes = {
-    '1': {name: 'in_bytes', len: 4, compileRule: decNumRule},
-    '2': {name: 'in_pkts', len: 4, compileRule: decNumRule},
-    '3': {name: 'flows', len: 4, compileRule: decNumRule},
-    '4': {name: 'protocol', len: 1, compileRule: decNumRule},
-    '5': {name: 'src_tos', len: 1, compileRule: decNumRule},
-    '6': {name: 'tcp_flags', len: 1, compileRule: decNumRule},
-    '7': {name: 'l4_src_port', len: 2, compileRule: decNumRule},
-    '8': {name: 'ipv4_src_addr', len: 4, compileRule: decIpv4Rule},
-    '9': {name: 'src_mask', len: 1, compileRule: decNumRule},
-    '10': {name: 'input_snmp', len: 2, compileRule: decNumRule},
-    '11': {name: 'l4_dst_port', len: 2, compileRule: decNumRule},
-    '12': {name: 'ipv4_dst_addr', len: 4, compileRule: decIpv4Rule},
-    '13': {name: 'dst_mask', len: 1, compileRule: decNumRule},
-    '14': {name: 'output_snmp', len: 2, compileRule: decNumRule},
-    '15': {name: 'ipv4_next_hop', len: 4, compileRule: decIpv4Rule},
-    '16': {name: 'src_as', len: 2, compileRule: decNumRule},
-    '17': {name: 'dst_as', len: 2, compileRule: decNumRule},
-    '18': {name: 'bgp_ipv4_next_hop', len: 4, compileRule: decIpv4Rule},
-    '19': {name: 'mul_dst_pkts', len: 4, compileRule: decNumRule},
-    '20': {name: 'mul_dst_bytes', len: 4, compileRule: decNumRule},
-    '21': {name: 'last_switched', len: 4, compileRule: decNumRule},
-    '22': {name: 'first_switched', len: 4, compileRule: decNumRule},
-    '23': {name: 'out_bytes', len: 4, compileRule: decNumRule},
-    '24': {name: 'out_pkts', len: 4, compileRule: decNumRule},
-    '25': {name: 'min_pkt_lngth', len: 2, compileRule: decNumRule},
-    '26': {name: 'max_pkt_lngth', len: 2, compileRule: decNumRule},
-    '27': {name: 'ipv6_src_addr', len: 16, compileRule: decIpv6Rule},
-    '28': {name: 'ipv6_dst_addr', len: 16, compileRule: decIpv6Rule},
-    '29': {name: 'ipv6_src_mask', len: 1, compileRule: decNumRule},
-    '30': {name: 'ipv6_dst_mask', len: 1, compileRule: decNumRule},
-    '31': {name: 'ipv6_flow_label', len: 3, compileRule: decNumRule},
-    '32': {name: 'icmp_type', len: 2, compileRule: decNumRule},
-    '33': {name: 'mul_igmp_type', len: 1, compileRule: decNumRule},
-    '34': {name: 'sampling_interval', len: 4, compileRule: decNumRule},
-    '35': {name: 'sampling_algorithm', len: 1, compileRule: decNumRule},
-    '36': {name: 'flow_active_timeout', len: 2, compileRule: decNumRule},
-    '37': {name: 'flow_inactive_timeout', len: 2, compileRule: decNumRule},
-    '38': {name: 'engine_type', len: 1, compileRule: decNumRule},
-    '39': {name: 'engine_id', len: 1, compileRule: decNumRule},
-    '40': {name: 'total_bytes_exp', len: 4, compileRule: decNumRule},
-    '41': {name: 'total_pkts_exp', len: 4, compileRule: decNumRule},
-    '42': {name: 'total_flows_exp', len: 4, compileRule: decNumRule},
-    '44': {name: 'ipv4_src_prefix', len: 4, compileRule: decIpv4Rule},
-    '45': {name: 'ipv4_dst_prefix', len: 4, compileRule: decIpv4Rule},
-    '46': {name: 'mpls_top_label_type', len: 1, compileRule: decIpv4Rule},
-    '47': {name: 'mpls_top_label_ip_addr', len: 4, compileRule: decIpv4Rule},
-    '48': {name: 'flow_sampler_id', len: 1, compileRule: decNumRule},
-    '49': {name: 'flow_sampler_mode', len: 1, compileRule: decNumRule},
-    '50': {name: 'flow_sampler_random_interval', len: 4, compileRule: decNumRule},
-    '52': {name: 'min_ttl', len: 1, compileRule: decNumRule},
-    '53': {name: 'max_ttl', len: 1, compileRule: decNumRule},
-    '54': {name: 'ipv4_ident', len: 2, compileRule: decNumRule},
-    '55': {name: 'dst_tos', len: 1, compileRule: decNumRule},
-    '56': {name: 'in_src_mac', len: 6, compileRule: decMacRule},
-    '57': {name: 'out_dst_mac', len: 6, compileRule: decMacRule},
-    '58': {name: 'src_vlan', len: 2, compileRule: decNumRule},
-    '59': {name: 'dst_vlan', len: 2, compileRule: decNumRule},
-    '60': {name: 'ip_protocol_version', len: 1, compileRule: decNumRule},
-    '61': {name: 'direction', len: 1, compileRule: decNumRule},
-    '62': {name: 'ipv6_next_hop', len: 16, compileRule: decIpv6Rule},
-    '63': {name: 'bpg_ipv6_next_hop', len: 16, compileRule: decIpv6Rule},
-    '64': {name: 'ipv6_option_headers', len: 4, compileRule: decNumRule},
-    '70': {name: 'mpls_label_1', len: 3, compileRule: decNumRule},
-    '71': {name: 'mpls_label_2', len: 3, compileRule: decNumRule},
-    '72': {name: 'mpls_label_3', len: 3, compileRule: decNumRule},
-    '73': {name: 'mpls_label_4', len: 3, compileRule: decNumRule},
-    '74': {name: 'mpls_label_5', len: 3, compileRule: decNumRule},
-    '75': {name: 'mpls_label_6', len: 3, compileRule: decNumRule},
-    '76': {name: 'mpls_label_7', len: 3, compileRule: decNumRule},
-    '77': {name: 'mpls_label_8', len: 3, compileRule: decNumRule},
-    '78': {name: 'mpls_label_9', len: 3, compileRule: decNumRule},
-    '79': {name: 'mpls_label_10', len: 3, compileRule: decNumRule},
-    '80': {name: 'in_dst_mac', len: 6, compileRule: decMacRule},
-    '81': {name: 'out_src_mac', len: 6, compileRule: decMacRule},
-    '82': {name: 'if_name', len: 2, compileRule: decStringRule},
-    '83': {name: 'if_desc', len: 4, compileRule: decStringRule},
-    '84': {name: 'sampler_name', len: 4, compileRule: decStringRule},
-    '85': {name: 'in_permanent_bytes', len: 4, compileRule: decNumRule},
-    '86': {name: 'in_permanent_pkts', len: 4, compileRule: decNumRule},
-    '88': {name: 'fragment_offset', len: 2, compileRule: decNumRule},
-    '89': {name: 'fw_status', len: 1, compileRule: decNumRule},
-    '90': {name: 'mpls_pal_rd', len: 8, compileRule: decNumRule},
-    '91': {name: 'mpls_prefix_len', len: 1, compileRule: decNumRule},
-    '92': {name: 'src_traffic_index', len: 4, compileRule: decNumRule},
-    '93': {name: 'dst_traffic_index', len: 4, compileRule: decNumRule},
-    '94': {name: 'application_descr', len: 4, compileRule: decStringRule},
-    '95': {name: 'application_tag', len: 4, compileRule: decMacRule},
-    '96': {name: 'application_name', len: 4, compileRule: decStringRule},
-    '98': {name: 'DiffServCodePoint', len: 1, compileRule: decNumRule},
-    '99': {name: 'replication_factor', len: 4, compileRule: decNumRule},
+    '1': {name: 'in_bytes', compileRule: decNumRule},
+    '2': {name: 'in_pkts', compileRule: decNumRule},
+    '3': {name: 'flows', compileRule: decNumRule},
+    '4': {name: 'protocol', compileRule: decNumRule},
+    '5': {name: 'src_tos', compileRule: decNumRule},
+    '6': {name: 'tcp_flags', compileRule: decNumRule},
+    '7': {name: 'l4_src_port', compileRule: decNumRule},
+    '8': {name: 'ipv4_src_addr', compileRule: decIpv4Rule},
+    '9': {name: 'src_mask', compileRule: decNumRule},
+    '10': {name: 'input_snmp', compileRule: decNumRule},
+    '11': {name: 'l4_dst_port', compileRule: decNumRule},
+    '12': {name: 'ipv4_dst_addr', compileRule: decIpv4Rule},
+    '13': {name: 'dst_mask', compileRule: decNumRule},
+    '14': {name: 'output_snmp', compileRule: decNumRule},
+    '15': {name: 'ipv4_next_hop', compileRule: decIpv4Rule},
+    '16': {name: 'src_as', compileRule: decNumRule},
+    '17': {name: 'dst_as', compileRule: decNumRule},
+    '18': {name: 'bgp_ipv4_next_hop', compileRule: decIpv4Rule},
+    '19': {name: 'mul_dst_pkts', compileRule: decNumRule},
+    '20': {name: 'mul_dst_bytes', compileRule: decNumRule},
+    '21': {name: 'last_switched', compileRule: decNumRule},
+    '22': {name: 'first_switched', compileRule: decNumRule},
+    '23': {name: 'out_bytes', compileRule: decNumRule},
+    '24': {name: 'out_pkts', compileRule: decNumRule},
+    '25': {name: 'min_pkt_lngth', compileRule: decNumRule},
+    '26': {name: 'max_pkt_lngth', compileRule: decNumRule},
+    '27': {name: 'ipv6_src_addr', compileRule: decIpv6Rule},
+    '28': {name: 'ipv6_dst_addr', compileRule: decIpv6Rule},
+    '29': {name: 'ipv6_src_mask', compileRule: decNumRule},
+    '30': {name: 'ipv6_dst_mask', compileRule: decNumRule},
+    '31': {name: 'ipv6_flow_label', compileRule: decNumRule},
+    '32': {name: 'icmp_type', compileRule: decNumRule},
+    '33': {name: 'mul_igmp_type', compileRule: decNumRule},
+    '34': {name: 'sampling_interval', compileRule: decNumRule},
+    '35': {name: 'sampling_algorithm', compileRule: decNumRule},
+    '36': {name: 'flow_active_timeout', compileRule: decNumRule},
+    '37': {name: 'flow_inactive_timeout', compileRule: decNumRule},
+    '38': {name: 'engine_type', compileRule: decNumRule},
+    '39': {name: 'engine_id', compileRule: decNumRule},
+    '40': {name: 'total_bytes_exp', compileRule: decNumRule},
+    '41': {name: 'total_pkts_exp', compileRule: decNumRule},
+    '42': {name: 'total_flows_exp', compileRule: decNumRule},
+    '44': {name: 'ipv4_src_prefix', compileRule: decIpv4Rule},
+    '45': {name: 'ipv4_dst_prefix', compileRule: decIpv4Rule},
+    '46': {name: 'mpls_top_label_type', compileRule: decIpv4Rule},
+    '47': {name: 'mpls_top_label_ip_addr', compileRule: decIpv4Rule},
+    '48': {name: 'flow_sampler_id', compileRule: decNumRule},
+    '49': {name: 'flow_sampler_mode', compileRule: decNumRule},
+    '50': {name: 'flow_sampler_random_interval', compileRule: decNumRule},
+    '52': {name: 'min_ttl', compileRule: decNumRule},
+    '53': {name: 'max_ttl', compileRule: decNumRule},
+    '54': {name: 'ipv4_ident', compileRule: decNumRule},
+    '55': {name: 'dst_tos', compileRule: decNumRule},
+    '56': {name: 'in_src_mac', compileRule: decMacRule},
+    '57': {name: 'out_dst_mac', compileRule: decMacRule},
+    '58': {name: 'src_vlan', compileRule: decNumRule},
+    '59': {name: 'dst_vlan', compileRule: decNumRule},
+    '60': {name: 'ip_protocol_version', compileRule: decNumRule},
+    '61': {name: 'direction', compileRule: decNumRule},
+    '62': {name: 'ipv6_next_hop', compileRule: decIpv6Rule},
+    '63': {name: 'bpg_ipv6_next_hop', compileRule: decIpv6Rule},
+    '64': {name: 'ipv6_option_headers', compileRule: decNumRule},
+    '70': {name: 'mpls_label_1', compileRule: decNumRule},
+    '71': {name: 'mpls_label_2', compileRule: decNumRule},
+    '72': {name: 'mpls_label_3', compileRule: decNumRule},
+    '73': {name: 'mpls_label_4', compileRule: decNumRule},
+    '74': {name: 'mpls_label_5', compileRule: decNumRule},
+    '75': {name: 'mpls_label_6', compileRule: decNumRule},
+    '76': {name: 'mpls_label_7', compileRule: decNumRule},
+    '77': {name: 'mpls_label_8', compileRule: decNumRule},
+    '78': {name: 'mpls_label_9', compileRule: decNumRule},
+    '79': {name: 'mpls_label_10', compileRule: decNumRule},
+    '80': {name: 'in_dst_mac', compileRule: decMacRule},
+    '81': {name: 'out_src_mac', compileRule: decMacRule},
+    '82': {name: 'if_name', compileRule: decStringRule},
+    '83': {name: 'if_desc', compileRule: decStringRule},
+    '84': {name: 'sampler_name', compileRule: decStringRule},
+    '85': {name: 'in_permanent_bytes', compileRule: decNumRule},
+    '86': {name: 'in_permanent_pkts', compileRule: decNumRule},
+    '88': {name: 'fragment_offset', compileRule: decNumRule},
+    '89': {name: 'fw_status', compileRule: decNumRule},
+    '90': {name: 'mpls_pal_rd', compileRule: decNumRule},
+    '91': {name: 'mpls_prefix_len', compileRule: decNumRule},
+    '92': {name: 'src_traffic_index', compileRule: decNumRule},
+    '93': {name: 'dst_traffic_index', compileRule: decNumRule},
+    '94': {name: 'application_descr', compileRule: decStringRule},
+    '95': {name: 'application_tag', compileRule: decMacRule},
+    '96': {name: 'application_name', compileRule: decStringRule},
+    '98': {name: 'DiffServCodePoint', compileRule: decNumRule},
+    '99': {name: 'replication_factor', compileRule: decNumRule},
     //above 127 is in ipfix
-    '128': {name: 'in_as', len: 4, compileRule: decNumRule},
-    '129': {name: 'out_as', len: 4, compileRule: decNumRule},
+    '128': {name: 'in_as', compileRule: decNumRule},
+    '129': {name: 'out_as', compileRule: decNumRule},
     //the following are taken from from http://www.iana.org/assignments/ipfix/ipfix.xhtml
-    '201': {name: 'mplsLabelStackLength', len: 4, compileRule: decNumRule}
+    '201': {name: 'mplsLabelStackLength', compileRule: decNumRule}
 };
 
 function nf9PktDecode(msg) {
@@ -207,19 +207,63 @@ function nf9PktDecode(msg) {
         return o;
     }
 
+    var nfScope = {
+        1: { name: 'scope_system', compileRule: decIpv6Rule },
+        2: { name: 'scope_interface', compileRule: decStringRule },
+        3: { name: 'scope_linecard', compileRule: decNumRule },
+        4: { name: 'scope_netflow_cache', compileRule: decNumRule },
+        5: { name: 'scope_template', compileRule: decStringRule }
+    };
+
+    function compileScope(type,pos,len) {
+        var nf = nfScope[type];
+        var cr = null;
+        if (nf.compileRule) {
+            cr = nf.compileRule[len] || nf.compileRule[0];
+            if (cr) {
+                return cr.toString().replace(/(\$pos)/g, function (n) {
+                    return pos
+                }).replace(/(\$len)/g, function (n) {
+                    return len
+                }).replace(/(\$name)/g, function (n) {
+                    return nf.name
+                });
+            }
+        }
+        debug('Unknown compile scope rule TYPE: %d POS: %d LEN: %d',type,pos,len);
+        return "";
+    }
+
     function readOptions(buffer) {
         var len = buffer.readUInt16BE(2);
         var tId = buffer.readUInt16BE(4);
         var osLen = buffer.readUInt16BE(6);
         var oLen = buffer.readUInt16BE(8);
-        var buf = buffer.slice(10, len - 10);
-        debug('readOptions: len:%d tId:%d osLen:%d oLen:%d',len,tId,osLen,oLen,buf);
+        var buff = buffer.slice(10,len);
+        debug('readOptions: len:%d tId:%d osLen:%d oLen:%d',len,tId,osLen,oLen,buff);
         var plen = 0;
         var cr = "var o={ isOption: true }; var t;\n";
+        var type; var tlen;
+
+        // Read the SCOPE
+        var buf = buff.slice(0,osLen);
         while (buf.length > 0) {
-            var type = buf.readUInt16BE(0);
-            var tlen = buf.readUInt16BE(2);
-            debug('    type: %d (%s) len: %d, plen: %d', type,nfTypes[type].name,tlen,plen);
+            while (buf.length > 0) {
+                type = buf.readUInt16BE(0);
+                tlen = buf.readUInt16BE(2);
+                debug('    SCOPE type: %d (%s) len: %d, plen: %d', type,nfTypes[type].name,tlen,plen);
+                cr+=compileScope(type, plen, tlen);
+                buf = buf.slice(4);
+                plen += tlen;
+            }
+        }
+
+        // Read the Fields
+        buf = buff.slice(osLen);
+        while (buf.length > 0) {
+            type = buf.readUInt16BE(0);
+            tlen = buf.readUInt16BE(2);
+            debug('    FIELD type: %d (%s) len: %d, plen: %d', type,nfTypes[type].name,tlen,plen);
             cr+=compileStatement(type, plen, tlen);
             buf = buf.slice(4);
             plen += tlen;
